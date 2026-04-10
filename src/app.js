9706ef81f7f02de5d2efee09adf25c430aa73d02
Anatoliy Korotenko <tolik.korotenko21@gmail.com>
2026-04-10 22:58:29 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const fs = require('fs');
+
+function cpFile() {
+  const [oldPath, newPath] = process.argv.slice(2);
+
+  if (!oldPath || !newPath) {
+    // eslint-disable-next-line no-console
+    console.error('Error not path');
+
+    return;
+  }
+
+  if (oldPath === newPath) {
+    // eslint-disable-next-line no-console
+    console.error('wrong  to copy to the same location');
+
+    return;
+  }
+
+  try {
+    fs.cpSync(oldPath, newPath);
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(error);
+  }
+}
+
+cpFile();
